0f346349d31d11723790dea52a6f913dd2e7ba94
atulraj <rajatul53@gmail.com>
2025-04-14 17:32:46 +0530

    Revert "ExpenseTracker V1 changes"
    This reverts commit ff83602887c5580dd94fb27e1025250f9cf7d094.

--- src/main/java/com/example/expensetracker/repo/ExpenseRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.expensetracker.repo;
-
-import com.example.expensetracker.entity.ExpenseEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ExpenseRepository extends JpaRepository<ExpenseEntity,Long> {
-}
